7dfd672e07b5b02b4c11d1325ddaf4a282e4c25f
Brian Bird <birdb@lanecc.edu>
2022-02-07 16:48:25 -0800

    Added Task.Run example

---
+++ TaskRun/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace TaskRun
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            double pi = await CalculatePi();
+            Console.WriteLine("Apporimation of pi: " + pi);
+        }
+
+        // Approximate pi using Wallis's Series
+        static async Task<double> CalculatePi()
+        {
+            const int N = 10000; // It takes 900 million iterations for 8 digits of precision
+            double pi = 4;
+
+            await Task.Run(() =>
+            {
+                for (double i = 3; i <= (N + 2); i += 2)
+                    pi = pi * ((i - 1) / i) * ((i + 1) / i);
+            });
+
+            return pi;
+        }
+    }
+}
+
+// Code for calculating pi was written by José Cintra
+// https://www.codeproject.com/Articles/813185/Calculating-the-Number-PI-Through-Infinite-Sequenc
